55475a26444070ee0aa3d4d92ebe80d5a19621b3
franklinp83 <61525814+franklinp83@users.noreply.github.com>
2024-12-11 19:06:12 -0600

    Agregar archivos de proyecto.

---
+++ Examen-Mvvm/ViewModels/Examen-MvvmViewModels.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+namespace Examen_Mvvm.ViewModels
+{
+    internal class Examen_MvvmViewModels : ObservableObject
+    {
+        [ObservableProperty]
+        private double producto1;
+
+        [ObservableProperty]
+        private double producto2;
+
+        [ObservableProperty]
+        private double producto3;
+
+        [ObservableProperty]
+        private double subTotal;
+
+        [ObservableProperty]
+        private double descuento;
+
+        [ObservableProperty]
+        private double totalPagar;
+        private void Alerta(string Titulo, string Mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Titulo, Mensaje, "Aceptar"));
+        }
+
+        [RelayCommand]
+        private void Calcular()
+        {
+            double totalSumaProductos = producto1 + producto2 + producto3;
+
+            try
+            {
+                if (totalSumaProductos >= 0 && totalSumaProductos <=999.99)
+                {
+                    descuento = 0;
+                }
+                else if (totalSumaProductos >= 1000 && totalSumaProductos <= 4999.99)
+                {
+                    descuento = totalSumaProductos * 0.10;
+                }
+                else if(totalSumaProductos >= 5000 && totalSumaProductos <= 9999.99)
+                {
+                    descuento = totalSumaProductos * 0.0;
+                }
+                else if(totalSumaProductos >= 10000)
+                {
+                    descuento = totalSumaProductos * 0.30;
+                }
+            }
+            catch (Exception ex)
+            {
+                Alerta("ERROR", ex.Message);
+            }
+
+            [RelayCommand]
+            private void Limpiar()
+            {
+                producto1 = 0;
+                producto2 = 0;
+                producto3 = 0;
+                descuento = 0;
+                subTotal = 0;
+                totalPagar = 0;
+
+            }
+        }
+    }
+}
---
+++ Examen-Mvvm/Views/Examen-MvvmView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Examen_Mvvm.Views;
+
+public partial class Examen_MvvmView : ContentView
+{
+	public Examen_MvvmView()
+	{
+		InitializeComponent();
+	}
+}
